d9994509841452bcffe884fb4e681e6a7a729a2f
patryks9090@gmail.com <Patryk0.1>
2021-04-12 08:28:29 +0200

    TAI_KOL1_gr3

---
+++ navbar.js
@@ -0,0 +1,8 @@
+let nav = '<nav> <ul>\n' +
+    '    <li><a href="index.html">Home</a></li>\n' +
+    '    <li><a href="kontakt.html">Kontakt</a></li>\n' +
+    '</ul></nav> ';
+
+let elem = document.querySelector(".nav-bar");
+
+elem.innerHTML = nav;
---
+++ script.js
@@ -0,0 +1,25 @@
+let div = document.querySelector(".box")
+let butt = document.querySelector(".button")
+let senderCounter;
+
+butt.addEventListener('click',function (){
+    console.log("asdasfasfg")
+    div.style.backgroundColor = "yellow"
+})
+
+let primary = document.querySelector(".primary")
+let danger = document.querySelector(".danger")
+ let inf = document.querySelector(".counter")
+
+
+ primary.addEventListener('click',function (){
+    if(senderCounter === null){
+        senderCounter=0
+    } else {
+        senderCounter++;
+    }
+    inf.textContent = senderCounter;
+})
+
+
+

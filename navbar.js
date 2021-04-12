8431bbf3973d2a11b27170b56987e91a29be4a9b
Marvel <codebreaker.mw@gmail.com>
2021-04-12 08:36:40 +0200

    kolos

---
+++ navbar.js
@@ -0,0 +1,5 @@
+let Nawigacja = '<nav><a href="index.html">Home</a>' + ' ' + '<a href="kontakt.html">Kontakt</a><nav>'+' '+'<nav><a href="formularz.html">Formularz</a>' + ' ';
+
+let navbarElem = document.querySelector(".nav-bar")
+
+navbarElem.innerHTML = Nawigacja;
---
+++ script.js
@@ -0,0 +1,9 @@
+let button = document.getElementById('buttonId');
+
+button.addEventListener('click',function(){
+
+    let color = 'red';
+
+    let box = document.getElementById('boxContainer');
+    box.style.background = color;
+})
